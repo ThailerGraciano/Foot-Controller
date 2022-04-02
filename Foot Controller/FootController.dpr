d82d447eab92a37a99e79cb9d2afca5f70ed4711
ThailerGraciano <thailer.thai@live.com>
2022-04-02 10:58:08 -0300

    01042022

--- Foot Controller/FootController.dpr
+++ Foot Controller/FootController.dpr
@@ -3,14 +3,14 @@ program FootController;
 uses
   Vcl.Forms,
   uPrincipal in 'uPrincipal.pas' {Form1},
-  uConfiguracoes in 'uConfiguracoes.pas' {Form2};
+  uConfiguracoes in 'uConfiguracoes.pas' {Form2},
+  uInicio in 'uInicio.pas' {Form3};
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
-  Application.CreateForm(TForm1, Form1);
-  Application.CreateForm(TForm2, Form2);
+  Application.CreateForm(TForm3, Form3);
   Application.Run;
 end.
---
+++ Foot Controller/uInicio.pas
@@ -0,0 +1,33 @@
+unit uInicio;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.Buttons;
+
+type
+  TForm3 = class(TForm)
+    Panel1: TPanel;
+    SpeedButton1: TSpeedButton;
+    procedure FormCreate(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form3: TForm3;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm3.FormCreate(Sender: TObject);
+begin
+  Form3.Width  := 1280;
+  Form3.Height := 720;
+end;
+
+end.
